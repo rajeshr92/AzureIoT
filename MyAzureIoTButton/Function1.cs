e7437c893d8b9f22821de4041fcfc2b0ffcb1e55
Unknown <rajeshr92@gmail.com>
2018-11-18 18:01:25 -0800

    Function1.cs
    INDENTATION ERROR

--- MyAzureIoTButton/Function1.cs
+++ MyAzureIoTButton/Function1.cs
@@ -19,7 +19,7 @@ namespace MyAzureIoTButton
         public static async System.Threading.Tasks.Task RunAsync([IoTHubTrigger("messages/events", Connection = "IoTHubConnectionString")]EventData message, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
-	    await client.PostAsync("https://prod-15.westus.logic.azure.com:443/workflows/01433f625eaf4a86a6ce165759a17c8f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=elEC2HJqRTa0kZbt8rAk3bGuiu2uvfTjZGRkJ-pkYaM", null);
+           await client.PostAsync("https://prod-15.westus.logic.azure.com:443/workflows/01433f625eaf4a86a6ce165759a17c8f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=elEC2HJqRTa0kZbt8rAk3bGuiu2uvfTjZGRkJ-pkYaM", null);
         }
     }
 
