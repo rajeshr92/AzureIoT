5782102888be15788ec3e46b891f0c020aba2014
Unknown <rajeshr92@gmail.com>
2018-11-18 18:16:04 -0800

    teXXmoButtonFunction
    Added comments

--- MyAzureIoTButton/teXXmoButtonFunction.cs
+++ MyAzureIoTButton/teXXmoButtonFunction.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
 
+// teXXmoAction is a function that receives communication from IoT Hub using the events end-point.
 
 namespace MyAzureIoTButton
 {
@@ -13,13 +14,15 @@ namespace MyAzureIoTButton
     public static class teXXmoAction
     {
 
-        private static HttpClient client = new HttpClient();
+        private static HttpClient client = new HttpClient(); // creating object of HttpClient
         [FunctionName("teXXmoAction")]
 
         public static async System.Threading.Tasks.Task RunAsync([IoTHubTrigger("messages/events", Connection = "IoTHubConnectionString")]EventData message, TraceWriter log)
         {
+            // logging the HTTP trigger and then calling the logic app hosted in Azure
             log.Info("C# HTTP trigger function processed a request.");
-           await client.PostAsync("https://prod-15.westus.logic.azure.com:443/workflows/01433f625eaf4a86a6ce165759a17c8f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=elEC2HJqRTa0kZbt8rAk3bGuiu2uvfTjZGRkJ-pkYaM", null);
+            // pointing to the URL where the Logic App is hosted
+            await client.PostAsync("https://prod-15.westus.logic.azure.com:443/workflows/01433f625eaf4a86a6ce165759a17c8f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=elEC2HJqRTa0kZbt8rAk3bGuiu2uvfTjZGRkJ-pkYaM", null);
         }
     }
 
